1bf14ada3a325a07fe0c64f43e36c1a2e12e52f4
Jack <gungravefire@yahoo.com>
2020-01-27 06:36:01 -0600

    Article detail template hook up to article through links

--- WebSite/djangonautic/articles/views.py
+++ WebSite/djangonautic/articles/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
-    return render(request, 'articles/article_list.html', {'articles': articles})
+    return render(request, 'articles/article_list.html', {'articles': articles}) # display html and with param 'articles'
 
-def article_detail(self, slug): #slug pass from url name capture group ?P<slug>
-    return HttpResponse(slug)
+def article_detail(request, slug): #slug pass from url name capture group ?P<slug>
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug) # query for Article with the same input slug
+    return render(request, 'articles/article_detail.html', {'article':article}) # 
--- WebSite/djangonautic/djangonautic/urls.py
+++ WebSite/djangonautic/djangonautic/urls.py
@@ -13,6 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+# flow of request
+# url.py->view.py->.html
+# model provide class with methods
+
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
