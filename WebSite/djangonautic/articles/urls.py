1483ebd0a0b48c06a2a55cb85c2228ffdb9c28a8
Jack <gungravefire@yahoo.com>
2020-01-26 21:19:24 -0600

    set named urls

--- WebSite/djangonautic/articles/urls.py
+++ WebSite/djangonautic/articles/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles' # namespace so html that calls list or detail know it's articles:list or articles:detail
+
 urlpatterns = [
-    url(r'^$', views.article_list),
+    url(r'^$', views.article_list, name="list"),
+    url(r'(?P<slug>[\w-]+)', views.article_detail, name="detail"), #(?P<slug>) name capture group; [\w-]+ means any a-Z,0-9,_, - hyphen included, + any length;  
 ]
