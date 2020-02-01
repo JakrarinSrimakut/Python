41881a3f704cd8d6762f716e4b9741fc42a4850b
Jack <gungravefire@yahoo.com>
2020-02-01 17:40:05 -0600

    create require login update

--- WebSite/djangonautic/articles/urls.py
+++ WebSite/djangonautic/articles/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 app_name = 'articles' # namespace so html that calls list or detail know it's articles:list or articles:detail
 
+#runs a url by top to bottom scheme
 urlpatterns = [
     url(r'^$', views.article_list, name="list"),
+    url(r'^create/$', views.article_create, name="create"),
     url(r'(?P<slug>[\w-]+)', views.article_detail, name="detail"), #(?P<slug>) name capture group; [\w-]+ means any a-Z,0-9,_, - hyphen included, + any length;  
 ]
--- WebSite/djangonautic/articles/views.py
+++ WebSite/djangonautic/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required #decorators extends functions
 
 # Create your views here.
 def article_list(request):
@@ -10,4 +11,9 @@ def article_list(request):
 def article_detail(request, slug): #slug pass from url name capture group ?P<slug>
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug) # query for Article with the same input slug
-    return render(request, 'articles/article_detail.html', {'article':article}) # 
+    return render(request, 'articles/article_detail.html', {'article':article}) 
+
+#Ensure login to access article_create redirect to login page if not
+@login_required(login_url="/accounts/login")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
