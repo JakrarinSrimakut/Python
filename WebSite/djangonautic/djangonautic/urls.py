0d6cb2ccf122d86a63eb1a48a30bcc030ca4b616
Jack <gungravefire@yahoo.com>
2020-02-07 17:25:18 -0600

    Redirect to Homepage

--- WebSite/djangonautic/djangonautic/urls.py
+++ WebSite/djangonautic/djangonautic/urls.py
@@ -24,13 +24,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static # allows django to serve media files
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
     url(r'articles/',include('articles.urls')),
     url(r'^about/$', views.about),
-    url(r'^$',views.homepage),
+    url(r'^$',article_views.article_list, name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns() #check if we're in debug mode then serve static files
